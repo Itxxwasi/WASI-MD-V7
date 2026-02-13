909a794a4de765b95aa048ec8b210a69f3c71d55
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2026-02-14 00:01:39 +0500

    Update Docker image in configuration

--- itxxwasi.js
+++ itxxwasi.js
@@ -12,7 +12,7 @@ const path = require('path');
 
 // Configuration
 const CONFIG = {
-    DOCKER_IMAGE: process.env.DOCKER_IMAGE || 'mrwasi/wasimdv7@sha256:8df63829675926a5eab84237702623ef2365f82a3e3eb4060b883677f2db707e',
+    DOCKER_IMAGE: process.env.DOCKER_IMAGE || 'mrwasi/sha256:4c0e06bfe4c36201d778420b85f063e3afe88a3594e7278461a2b6d116880de8',
     CONTAINER_NAME: 'wasi-md-v7-bot',
     MAX_RETRIES: 3,
     RETRY_DELAY: 5000, // 5 seconds
